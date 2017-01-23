f68edae5d938ae3679941af5c1d8c5ba2993b492
Tomy Varghese <anuatomy@gmail.com>
2017-01-23 14:08:40 -0500

    finish Fortune Cookie

--- main.py
+++ main.py
@@ -21,10 +21,8 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         header ="<h1>Fortune Cookie</h1>"
         lucky_number = random.randint(1,100)
-        number_sentence "Your lucky number: " + str(lucky_number)
+        number_sentence = 'Your lucky number: ' + str(lucky_number)
         number_parargaph = "<p>" + number_sentence + "</p>"
         self.response.write(header + number_parargaph)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+app = webapp2.WSGIApplication([('/', MainHandler)], debug = True)
